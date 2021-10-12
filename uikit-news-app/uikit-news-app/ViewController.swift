71f34c6ea2f7b48996403c4064104cb410cf4e99
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2021-10-12 13:44:21 +0700

    fix ios 10 compatibility, add image assets

---
+++ uikit-news-app/uikit-news-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-news-app
+//
+//  Created by Ahmad Syarifuddin Randiko on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
